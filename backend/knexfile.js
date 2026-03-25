26555bc41cbc0184e29c3298bf66dbd218d11977
VŨ Huy Đức <vuhuyduc@vuhuyduc1234dn@gmail.com>
2026-03-25 21:16:05 +0700

    HUYDUCK - Hoàn thiện 100% yêu cầu Tuần 08

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -10,9 +10,9 @@ export default {
     connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST || 'localhost',
       port: Number(process.env.DB_PORT) || 54232,
-      user: process.env.DB_USER || 'suser',
-      password: process.env.DB_PASSWORD || '123456',
-      database: process.env.DB_NAME || 'mydb',
+      user: process.env.DB_USER || 'admin',
+      password: process.env.DB_PASSWORD || 'admin',
+      database: process.env.DB_NAME || 'course_db',
     },
     pool: {
       min: 2,
